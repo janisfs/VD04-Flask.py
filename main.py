98a006b6d94baba9a656f6cb59d580ff0701fb35
janisfs <janisfs@mail.ru>
2024-09-06 22:04:07 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello world():
+def hello_world():
     return "Hello World!"
+
+@app.route("/new/")
+def new():
+    return "new page"
 
 if __name__ == "__main__":
     app.run()
