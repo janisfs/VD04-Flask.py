6a19e976d72ddbc9b7a83f4132d2bfee0271eb5d
janisfs <janisfs@mail.ru>
2024-09-07 23:02:43 +0300

    add new code

--- test-3.py
+++ test-3.py
@@ -2,12 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/<password>/")
-def hello_world(password=None):
-    if password == "12345":
-        return f"Добро пожаловать, мой друг!"
-    else:
-        return f"Неверный пароль!"
+def my_html_page():
+    html = """
+    <h2>Добро пожаловать, мой друг!</h2>
+    <p>Место для текста</p>
+    """
+    return html
 
 if __name__ == "__main__":
     app.run()
