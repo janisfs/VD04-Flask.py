183d2fdb44c4fd32c0e24c8bd939a3eba0200778
janisfs <janisfs@mail.ru>
2024-09-08 22:01:31 +0300

    Create test-4.py

---
+++ test-4.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return """
+    <h1>Тестовый запуск локального сервера</h1>
+    <p>А это просто текст</p>
+    """
+    return html
+
+if __name__ == "__main__":
+    app.run()
